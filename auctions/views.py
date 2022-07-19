18a0bf0da6b947f436429fe6cc457d3353d10e54
Gilbert Gotora <gilbertgotora2@gmail.com>
2022-07-19 07:06:38 +0200

    add ListingFoerm to forms.py

--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,8 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def new_listing(request):
+    return render(request, "auctions/new_listing.html", {
+        "form": form 
+    })
