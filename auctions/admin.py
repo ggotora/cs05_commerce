6934ee5ff771ff683644ade85a68ed616f93e6a1
Gilbert Gotora <gilbertgotora2@gmail.com>
2022-07-21 22:06:58 +0200

    reset db and  re-createsuperuser

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from auctions.models import Comment, Listing, Bid
+from auctions.models import Comment, Listing, Bid, User
 
 # Register your models here.
 admin.site.register(Listing)
 admin.site.register(Bid)
+admin.site.register(User)
 admin.site.register(Comment)
